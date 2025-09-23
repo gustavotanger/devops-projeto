ce7db9ccac0fcc13a9b1a4ef91cf97971501fd83
Gustavo Tanger <gugatanger@gmail.com>
2025-09-23 01:40:06 +0000

    Adiciona app_logic.py para testes no CI

---
+++ app_logic.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def is_even(n):
+    return n % 2 == 0
+
+def reverse_string(s):
+    return s[::-1]
+
+def capitalize_words(s):
+    return ' '.join(w.capitalize() for w in s.split())
+
+def divide(a, b):
+    return a / b
