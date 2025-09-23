bc4c2a9fa1410cfba4f6a2fd9e933c4348635d47
Gustavo Tanger <gugatanger@gmail.com>
2025-09-23 00:12:09 +0000

    Adiciona testes unitários #2

---
+++ tests/test_app_logic.py
@@ -0,0 +1,20 @@
+import pytest
+from app_logic import add, is_even, reverse_string, capitalize_words, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_is_even():
+    assert is_even(4)
+    assert not is_even(3)
+
+def test_reverse_string():
+    assert reverse_string("abc") == "cba"
+
+def test_capitalize_words():
+    assert capitalize_words("hello world") == "Hello World"
+
+def test_divide():
+    with pytest.raises(ZeroDivisionError):
+        divide(1, 0)
+    assert divide(6, 3) == 2
